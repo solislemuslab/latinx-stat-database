421872ca56c9b165c389b01d26dc454e185b637e
Reed Nelson <reed.nelson.d@gmail.com>
2021-09-29 22:44:15 -0500

    setup React app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Rome.com wasn't built in a single commit.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
