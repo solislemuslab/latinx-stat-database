590324d8721209ef9e63e94424016d8267a5ca42
Reed Nelson <reed.nelson.d@gmail.com>
2022-02-01 15:37:21 -0600

    Add Claudia's content

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,7 +1,7 @@
 // home.js
 import React, { Component } from "react";
 import asa from "./../img/asa-logo.png";
-// import wid from "./../img/wid-logo.png"; // is png?
+import wid from "./../img/wid-logo.png";
 import Google from "./google.js";
 
 export default class Home extends Component {
@@ -19,44 +19,35 @@ export default class Home extends Component {
         <div class="row">
           <div class="col-sm-7">
             <a
+              href="https://wid.wisc.edu/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img class="wid-logo" src={wid} alt="WID Logo"></img>
+            </a>
+            <a
               href="https://www.amstat.org/"
               target="_blank"
               rel="noopener noreferrer"
             >
               <img class="home-img" src={asa} alt="ASA Logo"></img>
             </a>
-            {/* <a
-              href="https://wid.wisc.edu/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img class="home-img" src={wid} alt="WID Logo"></img>
-            </a> */}
 
-            <h3 class="home-heading">
-              Welcome to the database for Latinx Statisticians and Data
-              Scientists
-            </h3>
+            <h3 class="home-heading">Welcome to the #LatinxInStat database</h3>
 
             <p class="home-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
+              Welcome to the Latinx in Statistics database hosted at the
+              Wisconsin Institute for Discovery (WID) in coordination with the
+              American Statistical Association (ASA). This database contains
+              information of Latinx statisticians and data scientists. If you
+              identify as Latinx, please add yourself to the database by logging
+              in with Google and creating your profile. If you are organizing a
+              statistical conference, please use this database to include a more
+              diverse representation of speakers.
             </p>
           </div>
           <div class="col-sm-5">
             <div class="btn-container-home">
-              {/* {if (localStorage.getItem("loginData")) {
-                if (JSON.parse(localStorage.getItem("loginData")).visible) {
-                  (<a
-                    class="btn btn-outline-primary btn-lg btn-people"
-                    href="/profile"
-                  >
-                    COMPLETE PROFILE SETUP
-                  </a>)
-                }
-              }} */}
               <a
                 class="btn btn-outline-primary btn-lg btn-people"
                 href="/table"
